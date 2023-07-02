dc0538183378cc58b0f3e27129f661d1d41d014c
Temm <samumert@gmail.com>
2023-07-02 16:31:42 +0200

    Add openai api cost calculation

--- src/invokegpt.ts
+++ src/invokegpt.ts
@@ -31,5 +31,11 @@ async function invokeModel(messages: ChatCompletionRequestMessage[]) {
     const message = data.choices[0].message;
     if (message?.role != "assistant" || !message?.content) throw new Error("Invalid response from LLM: " + JSON.stringify(message));
     console.log("Finsh reason:", data.choices[0].finish_reason)
+
+    // Pricing as of 2. July 2023: 0.3ct/1k Input, 0.4ct/1k Output
+    let ctCost = data.usage!.prompt_tokens * 0.0003 + data.usage!.completion_tokens * 0.0004; 
+    console.log("Tokens used: Input", data.usage!.prompt_tokens, "Output", data.usage!.completion_tokens);
+    console.log("Cost in cents:", Math.round(ctCost * 100) / 100);
+
     return message.content;
 }
